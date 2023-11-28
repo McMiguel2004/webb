833a210fcd3073f2068401c5f9af2fc272cb772c
McMiguel2004 <mcgomezalcantara@elpuig.xeill.net>
2023-11-28 15:46:23 +0100

    si

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,20 +22,31 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Esta clase extrae información de monstruos de un sitio web y genera archivos CSV y XML con los datos obtenidos.
+ */
 public class Main {
 
+    /**
+     * Método principal que inicia el proceso de extracción de datos.
+     *
+     * @param args Argumentos de línea de comandos (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
         try {
+            // Configuración del WebDriver para Firefox y navegación a la página web
             System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
             FirefoxOptions options = new FirefoxOptions();
             WebDriver driver = new FirefoxDriver(options);
             driver.get("https://monsterhunterworld.wiki.fextralife.com/Monsters");
             WebDriverWait wait = new WebDriverWait(driver, 10);
 
+            // Espera y aceptación del aviso de cookies
             WebElement botonAceptar = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div/div/div/div[2]/div/button[2]")));
             WebElement privacidad = driver.findElement(By.xpath("/html/body/div[1]/div/div/div/div[2]/div/button[2]"));
             privacidad.click();
 
+            // Extracción de enlaces de monstruos de la página principal
             List<String> links = new ArrayList<>();
             WebElement divElement = driver.findElement(By.xpath("(//div[@id='tagged-pages-container'])[1]"));
             List<WebElement> tittle = divElement.findElements(By.className("wiki_link"));
@@ -51,20 +62,23 @@ public class Main {
                 }
             }
 
-            // CSV File
+            // Creación del archivo CSV para almacenar los datos
             FileWriter csvWriter = new FileWriter("monstruos.csv");
             csvWriter.append("Nombre,Imagen,Descripcion\n");
 
+            // Creación del documento XML para almacenar los datos
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.newDocument();
             Element rootElement = doc.createElement("Monstruos");
             doc.appendChild(rootElement);
 
+            // Bucle para cada enlace de monstruo extraído
             for (String juegosLink : links) {
                 driver.get(juegosLink);
                 Thread.sleep(2000);
 
+                // Extracción de información de cada monstruo
                 WebElement tabla = driver.findElement(By.className("table-responsive"));
                 List<WebElement> tablilla = tabla.findElements(By.className("wiki_table"));
 
@@ -73,11 +87,12 @@ public class Main {
                     String imagen = nombrestexto.findElement(By.tagName("tbody")).findElement(By.tagName("img")).getAttribute("src");
                     String descripcion = driver.findElement(By.tagName("blockquote")).findElement(By.tagName("p")).getText();
 
-                    // Escribir en el archivo CSV de forma estructurada
+                    // Escritura de datos en el archivo CSV
                     csvWriter.append(nombre).append(",");
                     csvWriter.append(imagen).append(",");
                     csvWriter.append(descripcion).append("\n");
 
+                    // Estructuración del documento XML
                     Element monsterElement = doc.createElement("Monstruo");
                     rootElement.appendChild(monsterElement);
 
@@ -93,6 +108,7 @@ public class Main {
                     descriptionElement.appendChild(doc.createTextNode(descripcion));
                     monsterElement.appendChild(descriptionElement);
 
+                    // Extracción de enlaces adicionales dentro de la página del monstruo
                     List<String> linkstabla = new ArrayList<>();
                     WebElement divtabla = driver.findElement(By.className("wiki_table"));
                     List<WebElement> tittle2 = divtabla.findElements(By.className("wiki_link"));
@@ -101,11 +117,13 @@ public class Main {
                         linkstabla.add(nombresTabla.getAttribute("href"));
                     }
 
+                    // Bucle para cada enlace adicional
                     for (int i = 1; i < linkstabla.size(); i++) {
                         String tablaLink = linkstabla.get(i);
                         driver.get(tablaLink);
                         Thread.sleep(2000);
 
+                        // Extracción de información adicional y actualización del documento XML
                         WebElement blockquoteElement2 = driver.findElement(By.id("wiki-content-block"));
                         Element subDescriptionElement = doc.createElement("SubDescripcion");
                         subDescriptionElement.appendChild(doc.createTextNode(blockquoteElement2.findElement(By.tagName("p")).getText()));
@@ -114,10 +132,11 @@ public class Main {
                 }
             }
 
+            // Finalización y cierre del archivo CSV
             csvWriter.flush();
             csvWriter.close();
 
-            // XML Transformation
+            // Transformación y escritura del documento XML en un archivo
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -127,6 +146,7 @@ public class Main {
             StreamResult result = new StreamResult(new File("monstruos.xml"));
             transformer.transform(source, result);
 
+            // Cierre del navegador WebDriver
             driver.quit();
         } catch (Exception e) {
             e.printStackTrace();
